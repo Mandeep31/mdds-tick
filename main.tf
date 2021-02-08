7c7148ae241b631cf6661a4144a6349fd0f2dfd5
msdevops2017 <51741574+msdevops2017@users.noreply.github.com>
2021-02-08 19:36:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
-resource "rancher2_secret" "default" {
-  name          = "${var.name}"
-  description   = "Terraform key created for ${var.name}"
+resource "rancher2_secret" "rfw-data-platform-api-mdds-access1" {
+  name          = "data-platform-api-mdds-access-key-2"
+  description   = "Terraform access key for data platform api"
   project_id    = "${var.project_id}"
-  data          = "${var.data}"
+  data = {
+    data-platform-mdds-access = "${base64encode(chomp(file("/run/secrets/tf/rfw/rfw-dev-1-data-platform-mdds-access-key")))}"
+  }
 }
