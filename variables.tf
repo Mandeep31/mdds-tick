2dc11a9b1f8c34b9535dd3c8e94b4d6981dce469
msdevops2017 <51741574+msdevops2017@users.noreply.github.com>
2021-02-10 18:49:54 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,4 +2,16 @@ variable "project_id" {
   description = "Type the project id"
 }
 
+variable "env_number" {
+  description = "Env number"
+}
+
+variable "env_type" {
+  description = "Type of env"
+}
+
+variable "app_code" {
+  description = "App code"
+}
+
 
