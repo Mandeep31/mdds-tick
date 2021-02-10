bca760224e1a3574996d170ec6bf2c0220fe2631
msdevops2017 <51741574+msdevops2017@users.noreply.github.com>
2021-02-10 18:47:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,46 @@
-resource "rancher2_secret" "rfw-data-platform-api-mdds-access1" {
-  name          = "data-platform-api-mdds-access-key-2"
+resource "rancher2_secret" "rfw-data-platform-api-mdds-access" {
+  name          = "${var.env_number}-${var.env_type}-${var.app_code}-mdds-access-key"
   description   = "Terraform access key for data platform api"
-  project_id    = "${var.project_id}"
+  project_id    = "${module.riskframework_K8S_dev_10_project.project_id}"
   data = {
     data-platform-mdds-access = "${base64encode(chomp(file("/run/secrets/tf/rfw/rfw-dev-1-data-platform-mdds-access-key")))}"
   }
 }
+
+resource "rancher2_secret" "rfw-data-platform-api-mdds-secret" {
+  name          = "${var.env_number}-${var.env_type}-${var.app_code}-mdds-secret-key"
+  description   = "Terraform secret for data platform api"
+  project_id    = "${module.riskframework_K8S_dev_10_project.project_id}"
+  data = {
+    data-platform-mdds-secret = "${base64encode(chomp(file("/run/secrets/tf/rfw/rfw-dev-1-data-platform-mdds-access-secret")))}"
+  }
+}
+
+resource "rancher2_secret" "rfw-s3-mdds-bucket-user-access" {
+  name          = "${var.env_number}-${var.env_type}-${var.app_code}-s3-mdds-user-access-key"
+  description   = "Terraform access key for mdds S3 Bucket User"
+  project_id    = "${module.riskframework_K8S_dev_10_project.project_id}"
+  data = {
+    s3-access-mdds = "${base64encode(chomp(file("/run/secrets/tf/rfw/rfw-dev-1-s3-mdds-admin-access-id.txt")))}"
+  }
+}
+
+resource "rancher2_secret" "rfw-s3-mdds-bucket-user-secret" {
+  name          = "${var.env_number}-${var.env_type}-${var.app_code}-s3-mddds-user-secret-key"
+  description   = "Terraform secret for mdds S3 Bucket User"
+  project_id    = "${module.riskframework_K8S_dev_10_project.project_id}"
+  data = {
+    s3-secret-mdds = "${base64encode(chomp(file("/run/secrets/tf/rfw/rfw-dev-1-s3-mdds-admin-access-secret")))}"
+  }
+}
+
+
+resource "rancher2_secret" "rfw-dev-okta-svc-rnrk-mdds-secret" {
+  name = "${var.env_number}-${var.env_type}-${var.app_code}-rfw-dev-okta-svc-rnrk-mdds-secret"
+  description = "Okta secret for svc risk mdds"
+  project_id = "${module.riskframework_K8S_dev_10_project.project_id}"
+  data = {
+    okta-svc-rnrk-mdds-secret = "${base64encode(chomp(file("/run/secrets/tf/rfw/rfw-dev-okta-svc-rnrk-mdds-secret")))}"
+  }
+}
+
