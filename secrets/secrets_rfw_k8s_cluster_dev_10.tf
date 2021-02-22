a7cc86b4dcd54e2f239fb3d5d58e8b6cc209c307
msdevops2017 <51741574+msdevops2017@users.noreply.github.com>
2021-02-23 01:05:10 +0530

    Update secrets_rfw_k8s_cluster_dev_10.tf

--- secrets/secrets_rfw_k8s_cluster_dev_10.tf
+++ secrets/secrets_rfw_k8s_cluster_dev_10.tf
@@ -52,6 +52,18 @@ module "rfw-s3-sdp" {
   env_type   = "in2"
   app_code   = "6"
 }
+  
+module "rfw-data-api-platform" {
+  source = "git::https://github.theocc.com/platform-org/tf-modules-app.git//risk_framework/secrets/api_secrets?ref=master"
+  providers = {
+    rancher2 = "rancher2.dev"
+  }
+  project_id = "${module.riskframework_K8S_dev_10_project.project_id}"
+  env_number = "40"
+  env_type   = "in2"
+  app_code   = "6"
+
+}
 
 module "on-prem-kafka-keytab" {
   source = "git::https://github.theocc.com/platform-org/tf-modules-app.git//risk_framework/secrets/?ref=master"
